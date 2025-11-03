722cb753c2fea495eff7ee4e2e1d5fffa77477a6
boyke2448 <boyke2448@users.noreply.github.com>
2025-11-03 22:18:16 +0000

    Initial setup

---
+++ src/main.c
@@ -0,0 +1,24 @@
+#include "tcp.h"
+#include "main.h"
+
+int main() {
+        tcp_server server = {0};
+        server_status_e status = bind_tcp_port(&server, PORT);
+        if(status != SERVER_OK) {
+            printf("Server intialization failed.\n");
+            exit(EXIT_FAILURE);
+        }
+
+        int client_fd = accept_client(server.socket_fd);
+        if(client_fd == -1) {
+            printf("Faile to accept client connection.\n");
+            close(server.socket_fd);
+            exit(EXIT_FAILURE);
+        };
+
+        printf("Client connected.\n");
+
+        close(client_fd);
+        close(server.socket_fd);
+        return 0;
+}
